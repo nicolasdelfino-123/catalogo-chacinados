d8160eb2c9ab3f9432c6f4e719f99d42bf4f96c9
Nicolas <nicolasdelfino585@gmail.com>
2026-04-10 15:14:14 -0300

    se cambio persitentauth api url

--- frontend/src/utils/persistentAuth.js
+++ frontend/src/utils/persistentAuth.js
@@ -8,7 +8,7 @@
 
 const PERSISTENT_TOKEN_KEY = "persistent_token";
 const TOKEN_EXPIRY_KEY = "token_expiry";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_BACKEND_URL || "";
 
 /**
  * Login con sesión persistente (7 días sin caída)
